55f944c34d8e100e84d4fd6019937cc61806fdd2
sareyeh <sareyeh@gmail.com>
2020-08-12 14:16:40 -0400

    Initial commit

---
+++ medical-company-shipping/src/IShipment.java
@@ -0,0 +1,10 @@
+
+public interface IShipment {
+	
+	public boolean addShipment();
+	
+	public boolean deleteShipment();
+	
+	public boolean getShipment();
+	
+}
---
+++ medical-company-shipping/src/ShipmentServiceImpl.java
@@ -0,0 +1,22 @@
+
+public class ShipmentServiceImpl implements IShipment {
+
+	@Override
+	public boolean addShipment() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteShipment() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getShipment() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
